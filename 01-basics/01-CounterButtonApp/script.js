e6bb52c776d06d4b88f31b599c4cfddd5ee5bb78
Naurotski <6602796@gmail.com>
2022-01-30 17:45:45 +0100

    01-CounterButtonApp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,7 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+createApp({
+  data: () => ({
+    counter: 0,
+  }),
+}).mount('#app');
