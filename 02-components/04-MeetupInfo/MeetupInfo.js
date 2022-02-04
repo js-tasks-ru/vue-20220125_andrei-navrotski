010f3edccc1a72de503b116ab25fedefcdb880d1
Naurotski <6602796@gmail.com>
2022-02-04 20:15:08 +0100

    error correction

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -37,7 +37,7 @@ export default defineComponent({
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">{{ localDate }}</time>
+        <time datetime="2020-05-12">{{ localDate }}</time>
       </li>
     </ul>`,
 });
