4e57e9457427b6b4ec0d13dd859098b93ca2fa36
Naurotski <6602796@gmail.com>
2022-02-05 13:18:39 +0100

    error correction - 2

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -24,6 +24,9 @@ export default defineComponent({
         day: 'numeric',
       });
     },
+    isoDate() {
+      return new Date(this.date).toISOString().split('T')[0];
+    },
   },
   template: `
     <ul class="meetup-info">
@@ -37,7 +40,7 @@ export default defineComponent({
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-05-12">{{ localDate }}</time>
+        <time :datetime="isoDate">{{ localDate }}</time>
       </li>
     </ul>`,
 });
